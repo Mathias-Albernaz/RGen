7355ea965c8d15010d83190d0211e0037ec75ebe
Mathias-Albernaz <mathiasmatu15@gmail.com>
2024-06-28 19:30:40 -0300

    Se borra mudblazor

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,12 +1,12 @@
 using UserInterface.Components;
-using MudBlazor.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddMudServices();
+
 
 var app = builder.Build();
 
