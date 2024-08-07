722d24f965d95fb00e56e9c508db4edc67ecdac9
Mathias-Albernaz <mathiasmatu15@gmail.com>
2024-08-06 21:54:23 -0300

    se corrige implementacion de ReciboFactory.cs

--- Logica/ReciboFactory.cs
+++ Logica/ReciboFactory.cs
@@ -4,10 +4,14 @@ namespace Logica;
 
 public class ReciboFactory
 {
-      public void CrearRecibo(List<Item> items, Datos datos)
+      public int Id { get; set; } = 1;
+      public Recibo CrearRecibo(List<Item> items, Datos datos)
       {
             Recibo recibo = new Recibo(items, datos);
+            recibo.Id = Id;
             PlantillaRecibo plantillaRecibo = new PlantillaRecibo(recibo);
             plantillaRecibo.GenerarRecibo();
+            Id++;
+            return recibo;
       }
 }
