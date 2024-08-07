09e9e5cf78355091aaaaf240c3d3687b817122f4
Mathias-Albernaz <mathiasmatu15@gmail.com>
2024-08-06 21:37:59 -0300

    se implementa ReciboRepo con base de datos

---
+++ Repositorio/ReciboRepo.cs
@@ -0,0 +1,31 @@
+using Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositorio;
+
+public class ReciboRepo : IRepositorio<Recibo>
+{
+    private SqlContext _contexto;
+
+    public ReciboRepo(SqlContext contexto)
+    {
+        _contexto = contexto;
+    }
+    
+    public void Agregar(Recibo elemento)
+    {
+        _contexto.Add(elemento);
+    }
+
+    public void Borrar(int id)
+    {
+        _contexto.Remove(id);
+    }
+
+    public List<Recibo> Listar()
+    {
+        return _contexto.Recibos.Include(recibo => recibo.Items)
+            .Include(recibo => recibo.Datos)
+            .ToList();
+    }
+}
